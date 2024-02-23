75133d3a0a67854ba4dc5e0830752965f233a160
AggerGithub <aggercasbizy1209@gmail.com>
2024-02-23 16:01:45 +0300

    python week 1 assignment

--- user_input.py
+++
@@ -1,5 +0,0 @@
-name = input("What is your name? ")
-age = input("How old are you? ")
-location = input("Where are you from? ")
-
-print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
